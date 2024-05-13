4ca7374a0eef34dfe9c56d3f3dd96b032cbe6c25
Kaunda da Gama <dannydagamaisutic@gmail.com>
2024-05-13 23:43:43 +0100

    params

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent  implements OnInit{
+  constructor(private activedRoute: ActivatedRoute){
+
+  }
+  param:any;
+  queryParam:any;
+
+  ngOnInit(): void {
+    console.log(this.activedRoute);
+    this.param=this.activedRoute.snapshot.params['username']
+this.queryParam=this.activedRoute.snapshot.queryParams['course']
+
+
+  }
 
 }
